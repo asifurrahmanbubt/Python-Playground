d8aedd0b811bd40de3e1ee1a396519b802dc578c
Asifur Rahman <122219223+asifurrahmanbubt@users.noreply.github.com>
2025-01-31 19:33:34 +0600

    Add files via upload

---
+++ Variable/Variable.py
@@ -0,0 +1,9 @@
+character_name = "Atef"
+character_age = "23"
+character_goal = "To Learn Python"
+is_determined = True
+print("Who are you? " + character_name)
+print("How old are you? " + character_age)
+print("What is you objective? " + character_goal)
+print("Are you determined to complete this task? " + str(is_determined))
+print("Okay, Best of Luck")
